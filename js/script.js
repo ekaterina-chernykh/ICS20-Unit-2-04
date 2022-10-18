47efabafea7a4c6968cd91ba40bb6eb17e29326a
ekaterina-chern <113055918+ekaterina-chernykh@users.noreply.github.com>
2022-10-18 03:59:29 +0000

    .

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// Created by: Ekaterina
+// Created on: Oct 2022
+// This file contains the JS function for index.html
+
+"use strict"
+/**
+  * This function calculates area of triangle.
+  */
+function calculate() {
+  // input
+  const height = parseInt(document.getElementById("height-of-triangle").value)
+  const base = parseInt(document.getElementById("base-of-triangle").value)
+
+  // process
+  const area = (base * height) / 2
+
+  // output
+  document.getElementById("area").innerHTML = "Area is: " + area + "cm²"
+}
